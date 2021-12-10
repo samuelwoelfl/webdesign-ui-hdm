76460dee82e56d933417feb5dee2f46e6f8198dd
Samuel Wölfl <samuelwoelfl@gmx.de>
2021-12-10 16:55:11 +0100

    added theme switch

---
+++ js/themeSwitch.js
@@ -0,0 +1,34 @@
+$(document).ready(function() {
+  const $sun = $("#sun_icon");
+  const $moon = $("#moon_icon");
+
+  const html = document.querySelector('html');
+  const isOsDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+
+  if (isOsDark) {
+    switchTheme('dark');
+  } else {
+    switchTheme('light');
+  }
+
+  $sun.click(function() {
+    switchTheme('dark');
+  });
+  $moon.click(function() {
+    switchTheme('light');
+  });
+
+  function switchTheme(theme) {
+    if (theme === 'light') {
+      // Change Icon
+      $sun.show();
+      $moon.hide();
+    } else {
+      // Change Icon
+      $sun.hide();
+      $moon.show();
+    }
+    html.dataset.theme = `theme-${theme}`;
+  }
+
+});
