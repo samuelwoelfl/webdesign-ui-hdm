7e4181d8650b7eb763a21ec321c840866ad4b66d
Samuel Wölfl <samuelwoelfl@gmx.de>
2021-12-14 20:03:59 +0100

    only show welcome banner every x hours

--- js/cookies_script.js
+++ js/cookies_script.js
@@ -4,18 +4,31 @@ $(document).ready(function() {
   const $acceptCookiesButton = $("#accept_cookies");
   const $welcomeBanner = $("#welcome_banner");
 
-  var cookiesAccepted = false;
+
   var cookiesAccepted = Cookies.get('cookiesAccepted');
+  if (cookiesAccepted === undefined) {
+    var cookiesAccepted = false;
+  }
+
+  var hoursBetweenGreetings = 5;
+  var today = new Date();
+  var fallbackTime = today - (1000 * 60 * 60 * hoursBetweenGreetings)
+
+  var welcomeShownTime = Cookies.get('welcomeShownTime');
+  if (welcomeShownTime === undefined) {
+    var welcomeShownTime = fallbackTime;
+  }
 
   if (!cookiesAccepted) {
     $cookieBanner.delay(2000).fadeIn(100);
-  } else {
+  } else if (welcomeShownTime <= fallbackTime) {
     $welcomeBanner.delay(1000).fadeIn(100);
     $welcomeBanner.delay(2500).fadeOut(100);
+    Cookies.set('welcomeShownTime', Date.now());
   }
 
   $acceptCookiesButton.click(function() {
-    Cookies.set('cookiesAccepted', true)
+    Cookies.set('cookiesAccepted', true);
     $cookieBanner.hide();
   });
 
