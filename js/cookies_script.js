a06dfffb492ed89f7da1c4642ca0fb218220480c
Samuel Wölfl <samuelwoelfl@gmx.de>
2021-12-28 16:29:50 +0100

    fixed delay issue with snackbar

--- js/cookies_script.js
+++ js/cookies_script.js
@@ -1,30 +1,33 @@
 $(document).ready(function() {
 
+  // Alle Banner ausm HTML holen
   const $cookieBanner = $("#cookie_banner");
   const $acceptedBanner = $("#accepted_banner");
   const $deniedBanner = $("#denied_banner");
   const $welcomeBanner = $("#welcome_banner");
-
+  // Alle Buttons ausm HTML holen
   const $acceptCookiesButton = $("#accept_cookies");
   const $denieCookiesButton = $("#denie_cookies");
   const $resetCookiesButton = $("#reset_cookies");
 
 
-  var cookiesAccepted = Cookies.get('cookiesAccepted');
+  let cookiesAccepted = Cookies.get('cookiesAccepted');
   if (cookiesAccepted === undefined) {
     cookiesAccepted = false;
   }
 
-  hoursBetweenGreetings = 5;
-  var welcomeShown = Cookies.get('welcomeShown');
+  hoursBetweenGreetings = 1;
+  let welcomeShown = Cookies.get('welcomeShown');
   if (welcomeShown === undefined) {
     welcomeShown = false;
   }
 
   if (!cookiesAccepted) {
-    $cookieBanner.delay(2000).fadeIn(100);
+    setTimeout(function() {
+      $cookieBanner.fadeIn(100);
+    }, 1000);
   } else if (!welcomeShown) {
-    showBanner($welcomeBanner);
+    showToast($welcomeBanner);
     Cookies.set('welcomeShown', true, {
       expires: ((1 / 24) * hoursBetweenGreetings)
     });
@@ -33,30 +36,32 @@ $(document).ready(function() {
   $acceptCookiesButton.click(function() {
     Cookies.set('cookiesAccepted', true);
     $cookieBanner.fadeOut(100);
-    showBanner($acceptedBanner);
+    showToast($acceptedBanner);
   });
 
   $denieCookiesButton.click(function() {
     $cookieBanner.fadeOut(100);
-    showBanner($deniedBanner);
+    showToast($deniedBanner);
   });
 
   $resetCookiesButton.click(function() {
     $welcomeBanner.fadeOut(100);
     deleteAllCookies();
-    showBanner($deniedBanner);
+    showToast($deniedBanner);
   });
 });
 
-function showBanner($banner) {
-  var showDuration = $banner.attr("show-duration");
+function showToast($banner) {
+  let showDuration = $banner.attr("show-duration");
   $banner.find('.progress-ring_circle').css('animation-duration', `${showDuration / 1000}s`);
   $banner.fadeIn(100);
-  $banner.delay(showDuration).fadeOut(100);
+  setTimeout(function() {
+    $banner.fadeOut(100);
+  }, showDuration);
 }
 
 function deleteAllCookies() {
-  var cookies = Cookies.get();
+  let cookies = Cookies.get();
   for (const cookie in cookies) {
     Cookies.remove(cookie);
   }
