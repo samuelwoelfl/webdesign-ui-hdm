dbd07471bc9a2211233a27afc3f54ee1b1859546
Samuel Wölfl <samuelwoelfl@gmx.de>
2022-01-17 17:06:59 +0100

    fixed carousel and automatic scroll

--- js/carousel.js
+++ js/carousel.js
@@ -1,57 +1,89 @@
-$(document).ready(function() {
-  recCarouselEvents();
-});
+// set speed => metric is how many elements should be scrolled per click if all have the same size
+let speed = 1.5;
+// set if you want autoscroll and if so how many seconds should be between each scroll
+let autoScroll = true;
+let autoScrollSeconds = 3;
 
-function recCarouselEvents() {
-  let recTranslate = 0;
-  let carouselList = $('.carousel-list');
-  let listItems = $('.carousel-recs .carousel-listitem');
-  let itemsCount = listItems.length;
+// other variables for global scope
+let recTranslate = 0;
+let carouselList;
+let widthToScroll;
+let translateIncrem;
+let autoScrollInterval;
 
-  let singleItem = $(listItems[0]);
-  let singleItemWidth = singleItem.outerWidth(true);
-  let singleItemWidthWithoutMargin = singleItem.outerWidth(false);
+// window on load since we need to wait till the images are loaded here
+$(window).on("load", function() {
 
-  let margin = singleItemWidth - singleItemWidthWithoutMargin;
+  prevButton = $(".carousel-prev-rec");
+  nextButton = $(".carousel-next-rec");
 
-  let fullWidth = Math.round((singleItemWidth * listItems.length));
+  carouselList = $(".carousel-list");
+  listItems = $(".carousel-listitem");
+  itemsCount = listItems.length;
 
-  let visibleWidth = $('.carousel-body').outerWidth(true);
-  let visibleItems = visibleWidth / singleItemWidth;
-  let widthToScroll = Math.round(fullWidth - visibleWidth - margin);
-  let translateInrcem = widthToScroll / Math.round((itemsCount / 3));
-
-
-  $('.carousel-prev-rec').click(function() {
-    if (recTranslate < -1) {
-      recTranslate += translateInrcem;
-      carouselList.css('transform', 'translatex(' + recTranslate + 'px)');
-      $('.carousel-next-rec').addClass("active");
-      if (recTranslate > -1) {
-        $('.carousel-prev-rec').removeClass("active");
-      }
-    }
-  })
-
-  $('.carousel-next-rec').click(function() {
-    if (recTranslate >= ((widthToScroll * -1) + 5)) {
-      recTranslate -= translateInrcem;
-      carouselList.css('transform', 'translatex(' + recTranslate + 'px)');
-      $('.carousel-prev-rec').addClass("active");
-      if (recTranslate <= ((widthToScroll * -1) + 5)) {
-        $('.carousel-next-rec').removeClass("active");
-      }
-    }
+  let itemsFullWidth = 0;
+  $.each(listItems, function(i, item) {
+    itemsFullWidth += $(item).outerWidth(true);
   });
 
-  // setInterval(function() {
-  //   if (recTranslate >= ((widthToScroll * -1) + 5)) {
-  //     recTranslate -= translateInrcem;
-  //     carouselList.css('transform', 'translatex(' + recTranslate + 'px)');
-  //     $('.carousel-prev-rec').addClass("active");
-  //     if (recTranslate <= ((widthToScroll * -1) + 5)) {
-  //       $('.carousel-next-rec').removeClass("active");
-  //     }
-  //   }
-  // }, 3000);
+  let visibleWidth = $('.carousel-body').outerWidth(true);
+  widthToScroll = Math.round(itemsFullWidth - visibleWidth);
+  let targetIncrem = (itemsFullWidth / itemsCount) * speed;
+  let increments = Math.round(widthToScroll / targetIncrem);
+  // console.log(increments);
+  translateIncrem = (widthToScroll / increments);
+  // console.log(translateIncrem);
+
+
+  prevButton.click(function() {
+    if (recTranslate < 0) {
+      recTranslate += translateIncrem;
+      carouselList.css('transform', 'translatex(' + recTranslate + 'px)');
+    } else {
+      recTranslate = widthToScroll * -1;
+      carouselList.css('transform', 'translatex(' + recTranslate + 'px)');
+    }
+    pauseAutoScroll(5000);
+  });
+
+  nextButton.click(function() {
+    if (recTranslate > (widthToScroll * -1)) {
+      recTranslate -= translateIncrem;
+      carouselList.css('transform', 'translatex(' + recTranslate + 'px)');
+    } else {
+      recTranslate = 0;
+      carouselList.css('transform', 'translatex(' + recTranslate + 'px)');
+    }
+    pauseAutoScroll(5000);
+  });
+
+  activateAutoScroll(autoScroll, autoScrollSeconds);
+});
+
+
+function activateAutoScroll(activated, time) {
+  if (activated) {
+    console.log(autoScrollInterval);
+    autoScrollInterval = setInterval(function() {
+      if (recTranslate > (widthToScroll * -1)) {
+        recTranslate -= translateIncrem;
+        carouselList.css('transform', 'translatex(' + recTranslate + 'px)');
+      } else {
+        recTranslate = 0;
+        carouselList.css('transform', 'translatex(' + recTranslate + 'px)');
+      }
+    }, time * 1000);
+    console.log(autoScrollInterval);
+    console.log(typeof(autoScrollInterval));
+  }
+}
+
+function pauseAutoScroll(pauseTime) {
+  // FIXME: shoulnd't create multiple intervals when running during a pause
+  console.log(autoScrollInterval);
+  clearInterval(autoScrollInterval);
+  console.log(autoScrollInterval);
+  setTimeout(function() {
+    activateAutoScroll(autoScroll, autoScrollSeconds);
+  }, pauseTime);
 }
