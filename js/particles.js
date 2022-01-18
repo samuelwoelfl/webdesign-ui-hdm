42758a1178b60bab935982ef5bc22f796e2fc642
Samuel Wölfl <samuelwoelfl@gmx.de>
2022-01-18 10:47:16 +0100

    fixed theme switch for particles

--- js/particles.js
+++ js/particles.js
@@ -1,119 +1,124 @@
-let color;
-
 $(document).ready(function() {
-  let color = getComputedStyle(document.documentElement).getPropertyValue('--text-primary');
-  console.log(color);
+  let color = getComputedStyle(document.documentElement).getPropertyValue('--text-primary').trimStart();
+  console.log(`"${color}"`);
   var count_particles, stats, update;
 
   update = function() {
     requestAnimationFrame(update);
   };
   requestAnimationFrame(update);
-})
 
-info = {
-  particles: {
-    number: {
-      value: 30,
-      density: {
-        enable: true,
-        value_area: 800
-      }
-    },
-    color: {
-      value: "#FFFFFF"
-      // value: `${color}`
-      // FIXME: Variable color
-    },
-    shape: {
-      type: "circle",
-      stroke: {
-        width: 0,
-        color: "#000000"
-      },
-    },
-    opacity: {
-      value: 0.3,
-      random: false,
-      anim: {
-        enable: false,
-        speed: 1,
-        opacity_min: 0.1,
-        sync: false
-      }
-    },
-    size: {
-      value: 3,
-      random: true,
-      anim: {
-        enable: false,
-        speed: 40,
-        size_min: 0.1,
-        sync: false
-      }
-    },
-    line_linked: {
-      enable: true,
-      distance: 150,
-      color: "#ffffff",
-      opacity: 0.4,
-      width: 1
-    },
-    move: {
-      enable: true,
-      speed: 1.5,
-      direction: "none",
-      random: false,
-      straight: false,
-      out_mode: "out",
-      bounce: false,
-      attract: {
-        enable: false,
-        rotateX: 600,
-        rotateY: 1200
-      }
-    }
-  },
-  interactivity: {
-    detect_on: "canvas",
-    events: {
-      onhover: {
-        enable: true,
-        mode: "repulse"
-      },
-      onclick: {
-        enable: true,
-        mode: "push"
-      },
-      resize: true
-    },
-    modes: {
-      grab: {
-        distance: 400,
-        line_linked: {
-          opacity: 1
+
+  let info = {
+    particles: {
+      number: {
+        value: 30,
+        density: {
+          enable: true,
+          value_area: 800
         }
       },
-      bubble: {
-        distance: 400,
-        size: 40,
-        duration: 2,
-        opacity: 8,
-        speed: 3
+      color: {
+        value: `${color}`
       },
-      repulse: {
-        distance: 100,
-        duration: 0.4
+      shape: {
+        type: "circle",
+        stroke: {
+          width: 0,
+          color: "#000000"
+        },
       },
-      push: {
-        particles_nb: 4
+      opacity: {
+        value: 0.3,
+        random: false,
+        anim: {
+          enable: false,
+          speed: 1,
+          opacity_min: 0.1,
+          sync: false
+        }
       },
-      remove: {
-        particles_nb: 2
+      size: {
+        value: 3,
+        random: true,
+        anim: {
+          enable: false,
+          speed: 40,
+          size_min: 0.1,
+          sync: false
+        }
+      },
+      line_linked: {
+        enable: true,
+        distance: 150,
+        color: "#ffffff",
+        opacity: 0.4,
+        width: 1
+      },
+      move: {
+        enable: true,
+        speed: 1.5,
+        direction: "none",
+        random: false,
+        straight: false,
+        out_mode: "out",
+        bounce: false,
+        attract: {
+          enable: false,
+          rotateX: 600,
+          rotateY: 1200
+        }
       }
-    }
-  },
-  retina_detect: true
-}
+    },
+    interactivity: {
+      detect_on: "canvas",
+      events: {
+        onhover: {
+          enable: true,
+          mode: "repulse"
+        },
+        onclick: {
+          enable: true,
+          mode: "push"
+        },
+        resize: true
+      },
+      modes: {
+        grab: {
+          distance: 400,
+          line_linked: {
+            opacity: 1
+          }
+        },
+        bubble: {
+          distance: 400,
+          size: 40,
+          duration: 2,
+          opacity: 8,
+          speed: 3
+        },
+        repulse: {
+          distance: 100,
+          duration: 0.4
+        },
+        push: {
+          particles_nb: 4
+        },
+        remove: {
+          particles_nb: 2
+        }
+      }
+    },
+    retina_detect: true
+  }
 
-particlesJS("particles-js", info);
+  particlesJS("particles-js", info);
+
+  setTimeout(function() {
+    color = getComputedStyle(document.documentElement).getPropertyValue('--text-primary').trimStart();
+    info["particles"]["color"]["value"] = `${color}`;
+    info["particles"]["line_linked"]["color"] = `${color}`;
+    particlesJS("particles-js", info);
+  }, 2000)
+
+})
--- js/themeSwitch.js
+++ js/themeSwitch.js
@@ -1,16 +1,113 @@
 $(document).ready(function() {
 
-  // Beim Laden der Seite sollte die Theme Transition noch nicht smooth sondern möglichst unauffällig stattfinden und deswegen wird die transition erst nach dem Laden eingefügt
-  // setTimeout(function () {
-  //   $("body").css("transition","background-color .5s ease, color .5s ease");
-  // }, 100);
-
   // Elemente aus HTML bekommen
-  const $sun = $("#sun_icon");
-  const $moon = $("#moon_icon");
-  const html = document.querySelector('html');
+  let $sun = $("#sun_icon");
+  let $moon = $("#moon_icon");
+  let html = document.querySelector('html');
 
-  let theme;
+  let theme, color;
+  let info = {
+    particles: {
+      number: {
+        value: 30,
+        density: {
+          enable: true,
+          value_area: 800
+        }
+      },
+      color: {
+        value: "#FFFFFF"
+      },
+      shape: {
+        type: "circle",
+        stroke: {
+          width: 0,
+          color: "#000000"
+        },
+      },
+      opacity: {
+        value: 0.3,
+        random: false,
+        anim: {
+          enable: false,
+          speed: 1,
+          opacity_min: 0.1,
+          sync: false
+        }
+      },
+      size: {
+        value: 3,
+        random: true,
+        anim: {
+          enable: false,
+          speed: 40,
+          size_min: 0.1,
+          sync: false
+        }
+      },
+      line_linked: {
+        enable: true,
+        distance: 150,
+        color: "#FFFFFF",
+        opacity: 0.4,
+        width: 1
+      },
+      move: {
+        enable: true,
+        speed: 1.5,
+        direction: "none",
+        random: false,
+        straight: false,
+        out_mode: "out",
+        bounce: false,
+        attract: {
+          enable: false,
+          rotateX: 600,
+          rotateY: 1200
+        }
+      }
+    },
+    interactivity: {
+      detect_on: "canvas",
+      events: {
+        onhover: {
+          enable: true,
+          mode: "repulse"
+        },
+        onclick: {
+          enable: true,
+          mode: "push"
+        },
+        resize: true
+      },
+      modes: {
+        grab: {
+          distance: 400,
+          line_linked: {
+            opacity: 1
+          }
+        },
+        bubble: {
+          distance: 400,
+          size: 40,
+          duration: 2,
+          opacity: 8,
+          speed: 3
+        },
+        repulse: {
+          distance: 100,
+          duration: 0.4
+        },
+        push: {
+          particles_nb: 4
+        },
+        remove: {
+          particles_nb: 2
+        }
+      }
+    },
+    retina_detect: true
+  }
   let cookiesTheme = Cookies.get('theme');
   if (cookiesTheme === undefined) {
     let systemTheme;
@@ -29,6 +126,21 @@ $(document).ready(function() {
   } else {
     switchTheme('light');
   }
+
+
+  // ------------- Particle.js ------------------
+  color = getComputedStyle(document.documentElement).getPropertyValue('--text-primary').trimStart();
+  console.log(color);
+  var count_particles, stats, update;
+
+  update = function() {
+    requestAnimationFrame(update);
+  };
+  requestAnimationFrame(update);
+
+  particlesJS("particles-js", info);
+  // ------------ Particle.js End ----------------
+
 
   $sun.click(function() {
     switchTheme('dark');
@@ -51,6 +163,10 @@ $(document).ready(function() {
     if (Cookies.get('cookiesAccepted') == 'true') {
       Cookies.set('theme', `${theme}`);
     }
+    color = getComputedStyle(document.documentElement).getPropertyValue('--text-primary').trimStart();
+    info["particles"]["color"]["value"] = `${color}`;
+    info["particles"]["line_linked"]["color"] = `${color}`;
+    particlesJS("particles-js", info);
   }
 
 });
